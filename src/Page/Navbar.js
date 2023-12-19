74a3a56962cb99e7928cf7f26bf9ca5611d78cfd
atulgautamg <125824896+atulgautamg@users.noreply.github.com>
2023-12-19 08:06:41 +0530

    mainchange

--- src/Page/Navbar.js
+++ src/Page/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div>
          <nav className="navbar navbar-expand-lg bg-primary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">CrptoMania</Link>
+    <Link className="navbar-brand" to="/">CryptoMania</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
