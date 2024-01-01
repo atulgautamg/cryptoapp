0f76441fc91c2533385810ae62e2e322b9fb1928
atulgautamg <125824896+atulgautamg@users.noreply.github.com>
2024-01-02 01:30:13 +0530

    Update Banner.js

--- src/Page/Banner.js
+++ src/Page/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
     <div>
       <div className='techimg' style={{backgroundImage:`url(${techno})`,width:"100vw",height:"400px"}}>
         <div className='headh1'>
-        <h1> CyptoCurrency </h1>
+        <h1> CryptoCurrency </h1>
         </div>
         <div className='animate1'>
             
